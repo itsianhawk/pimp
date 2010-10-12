01532e070e08ad7c2f18b27694b1d98ace899c2e
Ian Hawkins <itsianhawk@gmail.com>
2010-10-12 12:45:03 -0400

    pimp program

---
+++ name.rb
@@ -0,0 +1,7 @@
+puts "hello there nigga what\'s your name?"
+name = gets.chomp
+puts "aight now what's yo bitches name?"
+bitch = gets.chomp
+puts "true true, now how much does she owe you?"
+money = gets.chomp
+puts bitch + " my name is " + name + " and you better have my " + money + " or i'mma wear yo ass out"
